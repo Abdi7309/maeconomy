b19a67c7cd4b0e8d359e617c3e0bc26f12d1218b
Abdi7309 <512875@vistacollege.nl>
2025-09-03 13:26:29 +0200

    Update API base URL and add default export to api.js
    Changed the API_BASE_URL in config.js to use a new IP address. Added a default export returning null to api.js.

--- app/(tabs)/api.js
+++ app/(tabs)/api.js
@@ -194,3 +194,5 @@ export const addProperties = async (objectId, properties) => {
         return false;
     }
 };
+
+export default () => null;
--- app/(tabs)/config/config.js
+++ app/(tabs)/config/config.js
@@ -1,7 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: 'http://10.3.1.59/Maeconomy/app/(tabs)/api.php',
-    DB_PORT: '3307', // Change this to '3307' for your friend
-    // Add other configuration settings as needed
+    API_BASE_URL: 'http://10.3.1.83/Maeconomy/app/(tabs)/api.php',
 };
 
 export default CONFIG;
