bebd764e57e07cd1ad0a77655e53ef7046481da8
Abdi7309 <512875@vistacollege.nl>
2026-01-05 13:43:03 +0100

    Remove unused config.js file
    Deleted app/(tabs)/config/config.js as Firebase configuration has been moved to config/firebase.js and this file is no longer needed.

--- app/(tabs)/config/config.js
+++
@@ -1,8 +0,0 @@
-// Firebase configuration is now in config/firebase.js
-// This file kept for backwards compatibility if needed elsewhere
-
-const CONFIG = {
-    // Non-Firebase configs can be added here if needed
-};
-
-export default CONFIG;
